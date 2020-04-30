4438197777d81ba8dca2989953b0f947ac36b364
Sergey <kvapuluk71@gmail.com>
2020-04-30 19:24:42 +0300

    Add comments

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using System; //Используем пространство имен System     
 
 namespace lab1
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello world");
+            Console.WriteLine("Hello world"); //Вызываем метод WriteLine, ничего не возвращает
         }
     }
 }
